b463601c8cee8cce869ed8b345c6149e54e3c436
Diego Moura <diegosouza20049@hotmail.com>
2015-07-06 20:13:41 -0300

    adicionado o Laravel FullCallendar

--- config/app.php
+++ config/app.php
@@ -152,6 +152,7 @@ return [
 		Laracasts\Flash\FlashServiceProvider::class,
 		Barryvdh\Debugbar\ServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
+        MaddHatter\LaravelFullcalendar\ServiceProvider::class,
 
     ],
 
@@ -200,6 +201,7 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+        'Calendar'  => MaddHatter\LaravelFullcalendar\Facades\Calendar::class,
 
 		/*
 		 * 3rd Party Aliases...
