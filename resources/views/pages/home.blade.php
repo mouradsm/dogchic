51d0d935e7d0577e3e2a74a09957ac93fad731ff
Diego Moura <diegosouza20049@hotmail.com>
2015-07-06 20:04:43 -0300

    Tela Home modificada

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,20 @@
+@extends('app')
+@section('title') Home :: @parent @stop
+@section('content')
+<div class="row">
+    <div class="page-header">
+        <h2>Calendário de Clubinhos</h2>
+    </div>
+</div>
+<div class="row">
+    Aqui vai o Calenrário
+</div>
+
+    
+
+@endsection
+
+@section('scripts')
+    @parent
+@endsection
+@stop
