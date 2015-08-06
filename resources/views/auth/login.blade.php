f7d730c09555caa8acf2f5a19c6db073f07b6dc1
Diego Moura <diegosouza20049@hotmail.com>
2015-08-06 15:51:01 -0300

    Começando a arrumar a casa

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link rel="stylesheet" href="../css/materialize.min.css">
-    <link rel="stylesheet" href="../assets/styles.css">
+    <link rel="stylesheet" href="../css/styles.css">
 </head>
 <body>
 <div class="container">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,16 +7,10 @@
 
     <title>DOGChic - Banho & Tosa</title>
 
-
-    <link href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en"
-          rel="stylesheet">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.3.2/fullcalendar.min.css" rel="stylesheet">
 
-    {{--    <link rel="stylesheet" href="assets/material.min.css">
-        <link rel="stylesheet" href="assets/styles.css">--}}
-
     <script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.3/moment.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.3.2/fullcalendar.min.js"></script>
 
@@ -38,7 +32,5 @@
     @yield('content')
 </div>
 
-
-{{--<script src="assets/material.min.js"></script>--}}
 </body>
 </html>
